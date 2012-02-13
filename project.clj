3f7aa5e5053e229929d06d653e6838fa1cc1739c
franks42 <frank.siebenlist@gmail.com>
2012-02-13 11:43:33 -0800

    version 1.9.0 to test if upgrade feature works

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-repls "1.6.0"
+(defproject lein-repls "1.9.0"
 	:description "A leiningen plugin to start a persistent repl server for use with cljsh"
 	:dependencies [	[org.clojure/clojure "1.3.0"]
 	                [hiccup "0.3.8"]
