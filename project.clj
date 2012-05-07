440ffa5c434e0e0ac7e7d92ce94d2def91cc5c1f
franks42 <frank.siebenlist@gmail.com>
2012-05-07 13:48:21 -0700

    added license info to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject lein-repls "1.9.8"
 	:description "A leiningen plugin to start a persistent repl server for use with cljsh"
-	:dependencies [[org.clojure/clojure "1.3.0"]
+  :url "https://github.com/franks42/lein-repls"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+ 	:dependencies [[org.clojure/clojure "1.3.0"]
 	               [clj-info "0.2.1"]
 	               [clj-growlnotify "0.1.0"]]
 	:dev-dependencies [[lein-marginalia "0.6.0"]
