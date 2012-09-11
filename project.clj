e13728261b724b8d401a876aedd4c8f0bb7d56bb
franks42 <frank.siebenlist@gmail.com>
2012-09-11 14:56:23 -0700

    1.9.9-SNAPSHOT update to clj-info 0.2.6

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject lein-repls "1.9.8"
+(defproject lein-repls "1.9.9-SNAPSHOT"
 	:description "A leiningen plugin to start a persistent repl server for use with cljsh"
   :url "https://github.com/franks42/lein-repls"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
- 	:dependencies [[org.clojure/clojure "1.3.0"]
-	               [clj-info "0.2.1"]
-	               [clj-growlnotify "0.1.0"]]
-	:dev-dependencies [[lein-marginalia "0.6.0"]
-  	                 [codox "0.5.0"]])
+ 	:dependencies [[org.clojure/clojure "1.4.0"]
+	               [clj-info "0.2.6"]
+	               [clj-growlnotify "0.1.2-SNAPSHOT"]]
+	:dev-dependencies [[lein-marginalia "0.7.1"]
+  	                 [codox "0.6.1"]])
--- src/cljsh/core.clj
+++ src/cljsh/core.clj
@@ -17,7 +17,7 @@
 
 ;; some set at lein-repls start in leiningen.repls/repl-server
 (def ^:dynamic cljsh.core/lein-repls-name "lein-repls") 
-(def ^:dynamic cljsh.core/lein-repls-version "1.9.8")
+(def ^:dynamic cljsh.core/lein-repls-version "1.9.9-SNAPSHOT")
 (def ^:dynamic cljsh.core/lein-project-name "") 
 (def ^:dynamic cljsh.core/lein-project-version "")
 (def ^:dynamic cljsh.core/lein-repls-host "")
